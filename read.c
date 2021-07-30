eb560a6c7e958db7ae5f740d52235f7668a3c80c
vignav <vignav.ram@gmail.com>
2021-07-30 16:34:56 +0530

    new file:   read.c

---
+++ read.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *file ;
+
+int main ( int argv , char **argc ){
+	
+	if ( argv !=2 ){
+		printf ( "Incorrect usage.\nUsage : read [file]\n" );
+		return ( 1 );
+	}
+	
+	char *file_name = argc[1];
+
+	if ( ( file = fopen ( file_name , "r" ) ) == NULL ){
+		printf ( "File does not exist.\n");
+		return ( 1 );
+	}
+
+	char *text = malloc ( sizeof ( char ) );
+	while ( !feof( file ) ){
+		fread ( text , sizeof ( char ) , 1 , file );
+		printf ( "%s" , text );
+
+	}
+	
+	free ( text );	
+	fclose ( file );	
+}
